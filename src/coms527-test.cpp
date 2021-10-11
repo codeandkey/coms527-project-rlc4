249045d294ce1d5c47649bfab3ab35af3ee5179a
Justin Stanley <jtst@iastate.edu>
2021-10-11 11:15:53 -0500

    bump cmake version, add gtest

---
+++ src/coms527-test.cpp
@@ -0,0 +1,11 @@
+/**
+ * This file does not contain anything -- the main function will be
+ * inserted automatically by gtest. Tests for components can be found along-
+ * side their definitions in the source.
+ */
+
+#include <gtest/gtest.h>
+
+TEST(TestSuiteTest, CanTest) {
+    EXPECT_EQ(0, 0);
+}
