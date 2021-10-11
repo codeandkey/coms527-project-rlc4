fdcb6ac339efad604a66870c679d6b7904df1164
Justin Stanley <jtst@iastate.edu>
2021-10-11 10:40:49 -0500

    add openmp build flags, basic omp test

--- src/coms527-app.cpp
+++ src/coms527-app.cpp
@@ -1,5 +1,18 @@
 #include <mpi.h>
+#include <omp.h>
+
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char** argv) {
+    #pragma omp parallel
+    {
+        #pragma omp critical
+        {
+            cout << "Hello from " << omp_get_thread_num() << " of " << omp_get_num_threads() << "!" << endl;
+        }
+    }
+
     return 0;
 }
