d2abc73822e962649b48ac50ab8df8c4face4c35
codeandkey <jtst@iastate.edu>
2021-12-01 13:09:06 -0600

    actual tensor type fix

--- drl/model.py
+++ drl/model.py
@@ -124,7 +124,7 @@ def train(trajectories):
         for i, (obs, mcts, result) in enumerate(loader, 0):
             optimizer.zero_grad()
 
-            obs = torch.tensor(obs, dtype=torch.double).cuda()
+            obs = torch.tensor(obs, dtype=torch.float32).cuda()
 
             policy, value = loaded(obs)
 
