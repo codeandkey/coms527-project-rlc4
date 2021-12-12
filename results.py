2e06862a124d97a92bce765aa92b1c4475bcf47b
Justin Stanley <jtst@iastate.edu>
2021-12-12 15:24:10 -0600

    reset scale in performance results

--- results.py
+++ results.py
@@ -30,7 +30,7 @@ axis[0].plot(loss_x, loss_y)
 
 axis[1].set_title('Evaluation score by generation')
 axis[1].plot(eval_x, eval_y, 'r')
-#axis[1].set_ylim(0, 1)
+axis[1].set_ylim(0, 1)
 
 plt.savefig('results.png')
 plt.show()
